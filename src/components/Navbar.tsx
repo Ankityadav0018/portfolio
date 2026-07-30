0922d25539ae1e2ae06b9ece3b02b16d0e638074
Rao Shab <raoshab@Raos-MacBook-Air.local>
2026-07-30 11:07:29 +0530

    Fix unused import ESLint errors

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useState, useEffect } from 'react';
-import { motion, AnimatePresence } from 'framer-motion';
+import { motion } from 'framer-motion';
 
 const navLinks = [
   { name: 'Home', href: '#hero' },
